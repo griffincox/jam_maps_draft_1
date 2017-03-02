26fc1bf947622f1a69bde14c0d7898934bbdb69f
Raghu Betina <raghu@firstdraft.com>
2017-03-02 02:11:54 +0000

    Whitelist columns in Instrument for ActiveAdmin

--- app/admin/instrument.rb
+++ app/admin/instrument.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Instrument do
 
+ permit_params :instrument_name
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
