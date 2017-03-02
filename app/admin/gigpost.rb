ffab0e581e481a257eb331cfa1f1b930e189ee50
Raghu Betina <raghu@firstdraft.com>
2017-03-02 02:11:45 +0000

    Whitelist columns in Gigpost for ActiveAdmin

--- app/admin/gigpost.rb
+++ app/admin/gigpost.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Gigpost do
 
+ permit_params :hourly_price, :title, :description, :band_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
